5c20918f713ba081e1ab617f63820869b6c74c5e
237121 <237121@KB37-118-C8>
2025-09-23 15:35:03 +0300

    Зпр ткст

--- ISIP423_Dautov/Program.cs
+++ ISIP423_Dautov/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class TextAnalyzer
+{
+    static List<TextStatistics> allStatistics = new List<TextStatistics>();
+
+    static void Main()
+    {
+        Console.WriteLine("Добро пожаловать в анализатор текста!");
+
+        while (true)
+        {
+            Console.WriteLine("\nВыберите действие:");
+            Console.WriteLine("1 - Анализ нового текста");
+            Console.WriteLine("2 - Просмотр статистики по прошлым текстам");
+            Console.WriteLine("3 - Выход");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    AnalyzeNewText();
+                    break;
+                case "2":
+                    ShowPreviousStatistics();
+                    break;
+                case "3":
+                    Console.WriteLine("До свидания!");
+                    return;
+                default:
+                    Console.WriteLine("Неверный выбор. Попробуйте снова.");
+                    break; }}}
+    static void AnalyzeNewText()
+    {
+        // Запрос текста у пользователя
+        string text;
+        do
+        {
+            Console.WriteLine("\nВведите текст (не менее 100 символов):");
+            text = Console.ReadLine();
+
+            if (text == null || text.Length < 100)
+            {
+                Console.WriteLine("Текст должен содержать не менее 100 символов. Попробуйте снова.");
+            }
+        } while (text == null || text.Length < 100);
