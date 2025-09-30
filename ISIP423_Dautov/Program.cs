8f6fead45d38e17412ce18de27ef62319edbb43b
237121 <237121@KB37-121-C06>
2025-09-30 13:30:09 +0300

    1

--- ISIP423_Dautov/Program.cs
+++ ISIP423_Dautov/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibraryApp
+{
+    public enum Genre
+    {
+        Fantasy,
+        ScienceFiction,
+        Mystery,
+        Romance,
+        Horror
+    }
