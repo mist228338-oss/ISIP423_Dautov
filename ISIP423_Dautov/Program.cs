7206db1bf69f041366e74136e706be1697eb5a3c
237121 <237121@KB37-121-C06>
2025-09-30 13:31:02 +0300

    11

--- ISIP423_Dautov/Program.cs
+++ ISIP423_Dautov/Program.cs
@@ -12,3 +12,13 @@ namespace LibraryApp
         Romance,
         Horror
     }
+    public class Book
+    {
+        private static int _nextId = 1;
+
+        public int Id { get; }
+        public string Title { get; }
+        public string Author { get; }
+        public Genre Genre { get; }
+        public int Year { get; }
+        public decimal Price { get; }
