5fa04ac9de6f64ffff5082b8364a0f422913294f
237121 <237121@KB37-118-C8>
2025-10-07 11:29:57 +0300

    111

--- ISIP423_Dautov/Program.cs
+++ ISIP423_Dautov/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿csharp
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Абстрактный класс Person (Абстракция)
+public abstract class Person
+{
+    protected string _name;
+    protected int _age;
+    protected string _email;
+    protected string _id;
+
+    public Person(string name, int age, string email)
+    {
+        _name = name;
+        _age = age;
+        _email = email;
+        _id = GenerateId();
+    }
+
+
